d92bae65be5fa3196e3b52701160e5ecd3557b12
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2022-09-28 20:57:56 +0300

    uppdate:  ready to sell

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
+import astro from "astro-compress";
+import sitemap from "@astrojs/sitemap";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+   site: 'https://supremeuikits.com',
+  integrations: [tailwind(), partytown(), astro(), sitemap(), image()]
 });

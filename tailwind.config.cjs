715d4c470646f5e9850c6a8b47f0c34a6d0dc320
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2023-01-18 13:12:12 +0200

    simple push

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,86 @@ const colors = require("tailwindcss/colors");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+     fontSize: {
+      xs: [
+        '0.75rem',
+        {
+          lineHeight: '1rem',
+        },
+      ],
+      sm: [
+        '0.875rem',
+        {
+          lineHeight: '1.5rem',
+        },
+      ],
+      base: [
+        '1rem',
+        {
+          lineHeight: '1.75rem',
+        },
+      ],
+      lg: [
+        '1.125rem',
+        {
+          lineHeight: '2rem',
+        },
+      ],
+      xl: [
+        '1.25rem',
+        {
+          lineHeight: '2rem',
+        },
+      ],
+      '2xl': [
+        '1.5rem',
+        {
+          lineHeight: '2rem',
+        },
+      ],
+      '3xl': [
+        '2rem',
+        {
+          lineHeight: '2.5rem',
+        },
+      ],
+      '4xl': [
+        '2.5rem',
+        {
+          lineHeight: '3.5rem',
+        },
+      ],
+      '5xl': [
+        '3rem',
+        {
+          lineHeight: '3.5rem',
+        },
+      ],
+      '6xl': [
+        '3.75rem',
+        {
+          lineHeight: '1',
+        },
+      ],
+      '7xl': [
+        '4.5rem',
+        {
+          lineHeight: '1.1',
+        },
+      ],
+      '8xl': [
+        '6rem',
+        {
+          lineHeight: '1',
+        },
+      ],
+      '9xl': [
+        '8rem',
+        {
+          lineHeight: '1',
+        },
+      ],
+    },
     extend: {
       backgroundImage: (theme) => ({
         desktopGradient: "url('./images/desktopGradient.mp4')",
@@ -13,21 +93,22 @@ module.exports = {
       },
       colors: {
         black:"#1b1d20",
-        blue: {
-          50: "#E5E5FF",
-          100: "#CCCCFF",
-          200: "#9999FF",
-          300: "#6666FF",
-          400: "#3333FF",
-          500: "#0000FF",
-          600: "#0000CC",
-          700: "#000099",
-          800: "#000066",
-          900: "#000033",
+        accent: {
+         50: "#EEE8FD",
+      100: "#E1D5FB",
+      200: "#C3ABF7",
+      300: "#A581F3",
+      400: "#8757EF",
+      500: "#672CEB",
+      600: "#4E13CD",
+      700: "#3B0F9A",
+      800: "#270A66",
+      900: "#140533"
         },
       },
       fontFamily: {
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        display: ["Neue Power", ...defaultTheme.fontFamily.sans],
       },
     },
   },

ae1b2de9d408c84db22cbbea2f991bd534997f4b
Volkan <volkan.kaya@hotmail.co.uk>
2023-07-12 07:39:06 +0100

    packages

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -162,7 +162,6 @@ module.exports = {
   },
   plugins: [
     require('@tailwindcss/typography'),
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/line-clamp")
+    require("@tailwindcss/forms")
   ],
 };
